889a9c01d5c7c70f9c856bcd735adec1d6746ebe
Stephen Edgar <stephen@netweb.com.au>
2014-02-16 12:14:27 +1100

    Updated list of hooks

--- init.php
+++ init.php
@@ -5,7 +5,7 @@ Plugin URI: http://jaredatchison.com/
 Description: Output the different bbPress hooks directly on your site for a quick and handy reference.
 Version: 1.2
 Author: Jared Atchison
-Author URI: http://jaredatchison.com 
+Author URI: http://jaredatchison.com
 */
 
 /**
@@ -13,7 +13,7 @@ Author URI: http://jaredatchison.com
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -43,7 +43,7 @@ class bbp_visual_hooks {
 		add_action( 'bbp_init',           array( $this, 'hook_list' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, 'styles'    ) );
 	}
-	
+
 	/**
 	 * styles()
 	 */
@@ -51,14 +51,14 @@ class bbp_visual_hooks {
 		wp_register_style( 'bbpress-visual-hooks', plugin_dir_url( __FILE__ ) . 'style.css' );
 	    wp_enqueue_style( 'bbpress-visual-hooks' );
 	}
-	
+
 	/**
 	 * hook_list()
 	 *
 	 * List of all hooks within bbp-theme-compat
 	 */
 	function hook_list() {
-		
+
 		$bbp_visual_hooks = array(
 			'bbp_after_main_content',
 			'bbp_after_popular_topics',
@@ -71,30 +71,44 @@ class bbp_visual_hooks {
 			'bbp_template_after_lead_topic',
 			'bbp_template_after_pagination_loop',
 			'bbp_template_after_replies_loop',
+			'bbp_template_after_search_results',
+			'bbp_template_after_search_results_loop',
+			'bbp_template_after_single_forum',
 			'bbp_template_after_single_reply',
 			'bbp_template_after_single_topic',
+			'bbp_template_after_topic_tag_description',
+			'bbp_template_after_topic_tag_edit',
 			'bbp_template_after_topics_index',
 			'bbp_template_after_topics_loop',
+			'bbp_template_after_user_details',
+			'bbp_template_after_user_favorites',
+			'bbp_template_after_user_profile',
+			'bbp_template_after_user_replies',
+			'bbp_template_after_user_subscriptions',
+			'bbp_template_after_user_topics_created',
 			'bbp_template_before_forums_index',
 			'bbp_template_before_forums_loop',
 			'bbp_template_before_lead_topic',
 			'bbp_template_before_pagination_loop',
 			'bbp_template_before_replies_loop',
+			'bbp_template_before_search',
+			'bbp_template_before_search_results_loop',
+			'bbp_template_before_single_forum',
 			'bbp_template_before_single_reply',
 			'bbp_template_before_single_topic',
+			'bbp_template_before_topic_tag_description',
 			'bbp_template_before_topic_tag_edit',
 			'bbp_template_before_topics_index',
 			'bbp_template_before_topics_loop',
-			'bbp_template_after_user_details',
 			'bbp_template_before_user_details',
 			'bbp_template_before_user_favorites',
-			'bbp_template_after_user_favorites',
+			'bbp_template_before_user_profile',
+			'bbp_template_before_user_replies',
 			'bbp_template_before_user_subscriptions',
-			'bbp_template_after_user_subscriptions',
 			'bbp_template_before_user_topics_created',
-			'bbp_template_after_user_topics_created',
 			'bbp_template_notices',
 			'bbp_theme_after_anonymous_form',
+			'bbp_theme_after_forum_content',
 			'bbp_theme_after_forum_description',
 			'bbp_theme_after_forum_form_content',
 			'bbp_theme_after_forum_form_parent',
@@ -106,6 +120,7 @@ class bbp_visual_hooks {
 			'bbp_theme_after_forum_form',
 			'bbp_theme_after_forum_freshness_link',
 			'bbp_theme_after_forum_sub_forums',
+			'bbp_theme_after_forum_subscription_action',
 			'bbp_theme_after_forum_title',
 			'bbp_theme_after_forum_visibility_status',
 			'bbp_theme_after_reply_admin_links',
@@ -128,6 +143,7 @@ class bbp_visual_hooks {
 			'bbp_theme_after_topic_form_content',
 			'bbp_theme_after_topic_form_forum',
 			'bbp_theme_after_topic_form_revisions',
+			'bbp_theme_after_topic_form_status',
 			'bbp_theme_after_topic_form_submit_button',
 			'bbp_theme_after_topic_form_submit_wrapper',
 			'bbp_theme_after_topic_form_subscriptions',
@@ -145,11 +161,11 @@ class bbp_visual_hooks {
 			'bbp_theme_anonymous_form_extras_bottom',
 			'bbp_theme_anonymous_form_extras_top',
 			'bbp_theme_before_anonymous_form',
+			'bbp_theme_before_forum_content',
 			'bbp_theme_before_forum_description',
 			'bbp_theme_before_forum_form_content',
 			'bbp_theme_before_forum_form_notices',
 			'bbp_theme_before_forum_form_parent',
-			'bbp_theme_before_forum_form_status',
 			'bbp_theme_before_forum_form_status',
 			'bbp_theme_before_forum_form_submit_button',
 			'bbp_theme_before_forum_form_submit_wrapper',
@@ -158,7 +174,9 @@ class bbp_visual_hooks {
 			'bbp_theme_before_forum_form',
 			'bbp_theme_before_forum_freshness_link',
 			'bbp_theme_before_forum_sub_forums',
+			'bbp_theme_before_forum_subscription_action',
 			'bbp_theme_before_forum_title',
+			'bbp_theme_before_reply_admin_links',
 			'bbp_theme_before_reply_author_admin_details',
 			'bbp_theme_before_reply_author_details',
 			'bbp_theme_before_reply_content',
@@ -180,6 +198,7 @@ class bbp_visual_hooks {
 			'bbp_theme_before_topic_form_forum',
 			'bbp_theme_before_topic_form_notices',
 			'bbp_theme_before_topic_form_revisions',
+			'bbp_theme_before_topic_form_status',
 			'bbp_theme_before_topic_form_submit_button',
 			'bbp_theme_before_topic_form_submit_wrapper',
 			'bbp_theme_before_topic_form_subscriptions',
@@ -199,32 +218,34 @@ class bbp_visual_hooks {
 			'bbp_user_edit_after_contact',
 			'bbp_user_edit_after_name',
 			'bbp_user_edit_after',
+			'bbp_user_edit_after_role',
 			'bbp_user_edit_before_about',
 			'bbp_user_edit_before_account',
 			'bbp_user_edit_before_contact',
 			'bbp_user_edit_before_name',
 			'bbp_user_edit_before',
+			'bbp_user_edit_before_role',
 			'login_form',
 			'register_form',
 		);
-		
+
 		foreach ( $bbp_visual_hooks as $hook ) {
 			add_action( $hook , array( $this, 'inject_hook' ) , 1 );
 		}
 
 	}
-	
+
 	/**
 	 * inject_hook()
 	 *
 	 * Injects the hook identifiers
 	 */
 	function inject_hook () {
-		
+
 		// The hooks can be temporarily hidden by passing 'show_hooks'
 		// Example: yourdomain.com/forums/?show_hooks=false
 		if ( !isset( $_GET['show_hooks'] ) ) {
-			
+
 			$current_action = current_filter();
 
 			echo '<div class="bbp-hook" title="' . $current_action . '">' . $current_action . '</div>';
